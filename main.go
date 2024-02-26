82921cd8cb2f75dd70765072b527f53e43e7b46d
LuckSaler <lucxsa@hotmail.com>
2024-02-26 14:52:49 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/salernolucas93/godesdecero/variables"
 )
 
 func main() {
 	variables.MostrarEnteros()
+	variables.RestoVariables()
+	estado, texto := variables.ConvertirTexto(200)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
